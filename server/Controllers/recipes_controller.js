a2ffdfd43ff7bfb7505c64813420b404a132ba6c
Lia Couch <76275077+Liakini2@users.noreply.github.com>
2021-02-02 18:13:58 -0700

    done

--- server/Controllers/recipes_controller.js
+++ server/Controllers/recipes_controller.js
@@ -2,86 +2,86 @@ recipes = [
     {
         id: 0,
         dishName: 'Deviled Eggs',
-        imgUrl: 'https://media.istockphoto.com/photos/deviled-eggs-picture-id174978970?k=6&m=174978970&s=612x612&w=0&h=w622ibQ7SHoWDw1UyCOANjM8L4K20hwnLXaxPTyhxZY=',
-        ingredientsNeeded: ['Eggs', 'Mayonaise', 'Bacon'],
-        instructions: "Boil eggs"
+        imgUrl: 'https://media.istockphoto.com/photos/deviled-eggs-with-paprika-picture-id491169864?k=6&m=491169864&s=612x612&w=0&h=l9a0j_eLGokB2tK49lbnPjS2q32c9bObKOWNqU6pfBk=',
+        ingredientsNeeded: ['Eggs', 'Mayo', 'Mustard', 'Bacon', 'Dill Weed', 'Celery Salt', 'Paprika'],
+        instructions: 'Boil eggs for 15 minutes. Once eggs have finished cooking cool down using cold water and peel. Cut egg in half and remove egg yokes into a large bowl. Cook bacon and cut into small pieces. Add to egg, seasonings, mayo, and mustard. Mix well and pipe back into egg white.'
     },
     {
         id: 1,
-        dishName: 'Chocolate Cake',
-        imgUrl: 'https://media.istockphoto.com/photos/confectioner-decorating-chocolate-cake-closeup-picture-id1187830875?s=170x170',
-        ingredientsNeeded: ['Flour', 'Egg', 'Coco Powder'],
-        instructions: 'Cook Cake'
+        dishName: 'Mushroom Chicken',
+        imgUrl: 'https://media.istockphoto.com/photos/chicken-marsala-picture-id962194078?k=6&m=962194078&s=612x612&w=0&h=jOk7cMb969YTB_uf26PlPoY5nYRSJixtNhcsrcQD5pY=',
+        ingredientsNeeded: ['Mushrooms', 'Chicken', 'Chicken Stock', 'Bread Crumbs', 'Garlic', 'Salt', 'Pepper'],
+        instructions: 'Saute garlic in oil for 3 minutes. Coat chicken in bread crumbs and add to pan. Once chicken is cooked remove from pan. Add Chicken Stock and mushrooms to pan and deglaze. Once mushrooms are soft add the chicken back into pan. Cook until well combined.'
     },
     {
         id: 2,
-        dishName: 'Spaghetti',
-        imgUrl: 'https://media.istockphoto.com/photos/pasta-plate-picture-id632439546?s=170x170',
-        ingredientsNeeded: ['Pasta', 'Tomatoe Sauce', 'Garlic'],
-        instructions: 'Boil Pasta'
+        dishName: 'Warm Apple Crisp',
+        imgUrl: 'https://media.istockphoto.com/photos/apple-crumble-picture-id467332803?k=6&m=467332803&s=612x612&w=0&h=njrajFeSb5N0QdLLAOygcNf1NPGemy5tsHi9-NjZpMo=',
+        ingredientsNeeded: ['Apples-Peeled and Chopped', 'Coconut-Milk', 'Cinnamon', 'Chopped Nuts', 'Butter', 'Ice Cream'],
+        instructions: 'In a large bowl, combine apples, coconut milk, and cinnamon. Spread mixture into a backing dish. Mix chopped nuts with melted butter. Sprinkle chopped nuts over the top. Bake in oven at 350 degrees for 20-25 minutes. Best served with Ice Cream.'
     },
     {
         id: 3,
         dishName: 'Tacos',
-        imgUrl: 'https://media.istockphoto.com/photos/soft-beef-tacos-with-fries-picture-id614313140?s=170x170',
+        imgUrl: 'https://media.istockphoto.com/photos/small-4inch-soft-beef-tacos-picture-id1146906219?k=6&m=1146906219&s=612x612&w=0&h=UNtK9oaNeIuVELKX_tcYemHvHDO82Um8hUKqqAaiMXY=',
         ingredientsNeeded: ['Tortillas', 'Ground Beef', 'Cheese', 'Letuce', 'Tomatoe', 'Sour Cream', 'Taco Seasoning', 'Onion'],
         instructions: 'Saute onion in oil for 3 minutes, add ground beef and seasoning and cook until beef has turned brown in color. Warm tortillas in microwave or on stove top. Assemble your taco with prefered toppings.'
     },
     {
         id: 4,
         dishName: 'Pumpkin Chili',
-        imgUrl: 'https://media.istockphoto.com/photos/latin-american-chilean-food-traditional-chilean-carbonara-stewed-beef-picture-id823104634?s=170x170',
+        imgUrl: 'https://media.istockphoto.com/photos/vegetarian-chili-posole-picture-id524528916?k=6&m=524528916&s=612x612&w=0&h=Hr5l8eiOX_l0Pi51SnAHHi6Xk-Qw9LdEqf7K5Yp8c6c=',
         ingredientsNeeded: ['Red Onion', 'Carrots', 'Garlic', 'Ground Beef', 'Pureed Pumpkin', 'Chicken Bone Broth'],
         instructions: 'Saute onion in oil for 3 minutes. Then add carrots, garlic, and pepper. Cook for another 2 minutes. Add the ground beef and cook until the meat turns brown. Add the remaining ingredients and stir well. Secure lid on instant pot and allow food to cook 20 to 30 minutes.'
     },
     {
         id: 5,
         dishName: 'Chicken Fried Rice',
-        imgUrl: 'https://media.istockphoto.com/photos/chicken-fried-rice-picture-id945606006?s=170x170',
+        imgUrl: 'https://media.istockphoto.com/photos/chicken-fried-rice-picture-id945606006?k=6&m=945606006&s=612x612&w=0&h=fZWJklaZpqhcVFU7v8Tc2d0S9OQu0pU9tvk9NxCira4=',
         ingredientsNeeded: ['Rice', 'Chicken', 'Egg', 'Vegetables', 'Soy Sauce', 'Ginger', 'Garlic'],
-        instructions: 'Cook white rice. In a skillet add ginger, garlic, and chicken. Once chicken is completely cooked place to the side. Add egg and vegetables. Cook until eggs are scrambled and vegetables are soft. Add all ingredients back into skillet, season to taste.'
+        instructions: 'Cook white rice. In a skillet add ginger, garlic, and chicken. Once chicken is completely cooked place to the side. Add egg and vegetables. Cook until eggs are scrambled and vegetables are soft. Add all ingredients back into skillet, season to taste. Serve with Rice.'
     },
     {
         id: 6,
         dishName: 'Crepes',
-        imgUrl: 'https://media.istockphoto.com/photos/crepes-with-chocolate-sauce-and-powdered-sugar-picture-id811988342?k=6&m=811988342&s=612x612&w=0&h=hchfqf3fJaKNs5j1IDIZSnoZ78cdAyvZgF9qYK-wJW8=',
-        ingredientsNeeded: ['Flour', 'Water', 'Egg', 'Vanilla'],
-        instructions: 'Savory or Sweet, Yummy Treat'
+        imgUrl: 'https://media.istockphoto.com/photos/thin-crepes-with-orange-sauce-picture-id958205114?k=6&m=958205114&s=612x612&w=0&h=E1dYDtTiftgTyXOZMdYE18KylnRv5rX4l0Hp4N9N8Pw=',
+        ingredientsNeeded: ['Milk', 'Flour', 'Eggs', 'Oil', 'Sugar', 'Vanilla'],
+        instructions: 'Place all ingredients into a blender and blend for 5 minutes. Heat a small, shallow pan on medium-low and grease with butter. Slowly pour batter into pan until the bottom of the pan in covered. Let cook for 2 minutes and flip. Cook 2 more minutes. Serve with fruit.'
     },
     {
         id: 7,
         dishName: 'Spaghetti',
-        imgUrl: 'https://media.istockphoto.com/photos/pasta-plate-picture-id632439546?s=170x170',
-        ingredientsNeeded: ['Pasta', 'Tomatoe Sauce', 'Garlic'],
-        instructions: 'Boil Pasta'
+        imgUrl: 'https://media.istockphoto.com/photos/spaghetti-bolognese-with-minced-beef-and-tomato-sauce-garnished-with-picture-id1203110818?k=6&m=1203110818&s=612x612&w=0&h=xvYyhMm55egu0PdV3QMBk6J0-FPGi0IgmLJ5Avmv6x8=',
+        ingredientsNeeded: ['Pasta', 'Tomato Sauce', 'Garlic', 'Ground Beef'],
+        instructions: 'Saute Garlic for 2 minutes. Add Ground Beef and cook until brown. Add tomato sauce and seasonings of choice and cook until simmering. In a large pot bring salted water to a boil. Add pasta and cook until al dente. Best served with garlic bread.'
     },
     {
         id: 8,
-        dishName: 'Spaghetti',
-        imgUrl: 'https://media.istockphoto.com/photos/pasta-plate-picture-id632439546?s=170x170',
-        ingredientsNeeded: ['Pasta', 'Tomatoe Sauce', 'Garlic'],
-        instructions: 'Boil Pasta'
+        dishName: 'Chili-Lime Shrimp',
+        imgUrl: 'https://media.istockphoto.com/photos/grilled-shrimp-skewers-picture-id955813762?k=6&m=955813762&s=612x612&w=0&h=yU7D-Kn5K0IeqS1j9L1ENLWcJpZwS-pOPGFgKslPA3A=',
+        ingredientsNeeded: ['Shrimp', 'Lime Juice', 'Chili Powder', 'Garlic Powder', 'Salt', 'Pepper'],
+        instructions: 'Heat pan to medium heat. in a large bowl combine the shrimp, oil, lime juice, chili powder, garlic powder, and a pinch of salt and pepper to taste. Thread shrimp through metal skewers and cook for 3-4 minutes per side.'
     },
     {
         id: 9,
-        dishName: 'Spaghetti',
-        imgUrl: 'https://media.istockphoto.com/photos/pasta-plate-picture-id632439546?s=170x170',
-        ingredientsNeeded: ['Pasta', 'Tomatoe Sauce', 'Garlic'],
-        instructions: 'Boil Pasta'
+        dishName: 'Asian Style Salmon',
+        imgUrl: 'https://media.istockphoto.com/photos/teriyaki-salmon-on-the-white-plate-picture-id1160026931?k=6&m=1160026931&s=612x612&w=0&h=DN4A4gIX7FW2PUuyIWIErKurGpt1Gl5H-0W6lteOiws=',
+        ingredientsNeeded: ['Salmon', 'Green Onion', 'Garlic', 'Chili Paste', 'Ginger', 'Soy Sauce', 'Mirin', 'Hosin Sauce'],
+        instructions: 'Season salmon with salt and pepper. In an oiled pan cook salmon until cooked through. Finely chop green onion. In a medium bowl combine the remaining ingredients. Plate salmon and spread sauce generously over the top. Serve with rice and steamed vegetables.'
     },
     {
         id: 10,
-        dishName: 'Spaghetti',
-        imgUrl: 'https://media.istockphoto.com/photos/pasta-plate-picture-id632439546?s=170x170',
-        ingredientsNeeded: ['Pasta', 'Tomatoe Sauce', 'Garlic'],
-        instructions: 'Boil Pasta'
+        dishName: 'Pork Chops',
+        imgUrl: 'https://media.istockphoto.com/photos/grilled-pork-chop-with-spices-picture-id1218397985?k=6&m=1218397985&s=612x612&w=0&h=7tF8O3T54BIegHabXr-SDqPJ8e72Jc1pVseYidJr75U=',
+        ingredientsNeeded: ['Pork', 'Thyme', 'Garlic', 'Salt', 'Pepper', 'Butter'],
+        instructions: 'Season pork using salt and pepper and bring to room temperature. Heat pan to medium heat and add butter, thyme and garlic. Saute for 1-2 minutes. Add pork and cook thoroughly making sure pan is hot enough to get a nice sear on the meat. Best served with veggies or rice.'
     },
     {
         id: 11,
-        dishName: 'Spaghetti',
-        imgUrl: 'https://media.istockphoto.com/photos/pasta-plate-picture-id632439546?s=170x170',
-        ingredientsNeeded: ['Pasta', 'Tomatoe Sauce', 'Garlic'],
-        instructions: 'Boil Pasta'
+        dishName: 'Chocolate Chip Cookies',
+        imgUrl: 'https://media.istockphoto.com/photos/milk-chocolate-macadamia-nut-cookies-and-milk-picture-id1178421338?k=6&m=1178421338&s=612x612&w=0&h=qIwlwWnWJdK-jOZuJBtPNZ1FB9F0Muh9WZOvG5Awtnw=',
+        ingredientsNeeded: ['Chocolate Chips', 'Butter', 'Egg', 'Vanilla', 'Sugar', 'Brown Sugar', 'Flour'],
+        instructions: 'Mix egg, vanilla, and butter in a large bowl. Add sugar and brown sugar and Mix. Slowly add in flour mixing as you add the flour. Add chocolate chips and stir. Roll into tablespoon sized balls and place on cookie sheet. Cook for 8-11 minutes at 375 degrees.'
     },
 ]
 id=12
